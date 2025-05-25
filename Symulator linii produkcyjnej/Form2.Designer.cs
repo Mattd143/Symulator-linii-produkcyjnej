3f1879387bf7b7edeba04006181467e09626de26
Mati <Mati@MATI-KOMPUTER>
2025-05-26 00:17:35 +0200

    Dodana symulacja

--- Symulator linii produkcyjnej/Form2.Designer.cs
+++ Symulator linii produkcyjnej/Form2.Designer.cs
@@ -37,6 +37,16 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
             this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label10 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -97,7 +107,7 @@
             // 
             this.button1.BackColor = System.Drawing.Color.IndianRed;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(200, 315);
+            this.button1.Location = new System.Drawing.Point(202, 315);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(230, 69);
             this.button1.TabIndex = 5;
@@ -110,12 +120,120 @@
             // 
             this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(385, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 25);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "60 °C";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(361, 187);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(104, 25);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "1200 RPM";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(385, 236);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 25);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "50 %";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(380, 287);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(78, 25);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "100 bar";
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label10.Location = new System.Drawing.Point(12, 102);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(75, 25);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "label10";
+            this.label10.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(510, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 37);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(279, 179);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(48, 37);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(464, 277);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 46);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Zawór bezpieczeństwa";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(464, 228);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(140, 46);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Tryb oszczędny";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(627, 396);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -139,5 +257,15 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
--- Symulator linii produkcyjnej/Form2.cs
+++ Symulator linii produkcyjnej/Form2.cs
@@ -15,17 +15,24 @@ namespace Symulator_linii_produkcyjnej
         
         private Timer zegarawarii = new Timer();
         private Timer zegarobecnosci = new Timer();
+        private Timer zegarsymulacji = new Timer();
         private Random rng = new Random();
-        public bool nieobecnosc = false;
-        
+        private bool nieobecnosc = false;
+        private int temperatura = 60;
+        private int obroty = 1200;
+        private int cpu = 50;
+        private int cisnienie = 100;
+
         public Main(string uzytkownik)
         {
             InitializeComponent();
             label1.Text = $"Zalogowany operator: {uzytkownik}";
             zegarawarii.Tick += timer1_Tick;
             zegarobecnosci.Tick += timer2_Tick;
+            zegarsymulacji.Tick += timer3_Tick;
             LosoweAwarie();
             Obecnosc();
+            Symulacja();
 
         }
 
@@ -38,10 +45,72 @@ namespace Symulator_linii_produkcyjnej
 
         public void Obecnosc()
         { 
-            zegarobecnosci.Interval = 10000;
+            zegarobecnosci.Interval = 25000;  // co 25 sekund trzeba potwierdzic obecnosc
             zegarobecnosci.Start();
         }
 
+        public void Symulacja()
+        {
+            zegarsymulacji.Interval = 2000;  // zmienia zmienne co 2 sekundy
+            zegarsymulacji.Start();
+            
+            // Symulacja zmiennych
+            temperatura += rng.Next(-1, 5);
+            obroty += rng.Next(-10, 10);
+            if (cpu > 35)
+            {
+                cpu += rng.Next(-2, 5);
+            }
+            else
+            {
+                cpu += rng.Next(0, 5);
+            }
+            cisnienie += rng.Next(0, 2);
+
+            // Wypisywanie nowych zmiennych
+            label6.Text = $"{temperatura} °C";
+            label7.Text = $"{obroty} RPM";
+            label8.Text = $"{cpu} %";
+            label9.Text = $"{cisnienie} bar";
+
+            // krytyczne wartosci - awarie
+            if (temperatura > 80)
+            {
+                label10.Visible = true;
+                label10.Text = $"UWAGA: Temperatura zbyt wysoka! Zwiększ obroty wentylatorów";
+            }
+            else if (temperatura < 40)
+            {
+                label10.Visible = true;
+                label10.Text = $"UWAGA: Temperatura zbyt niska! Zmniejsz obroty wentylatorów";
+            }
+            else if (cisnienie > 120)
+            {
+                label10.Visible= true;
+                label10.Text = $"UWAGA: Ciśnienie zbyt wysokie! Otwórz zawór bezpieczeństwa";
+            }
+            else if (cpu > 70)
+            {
+                label10.Visible = true;
+                button5.Visible = true;
+                label10.Text = $"UWAGA: Wykorzystanie cpu zbyt wysokie! Włącz tryb oszczędny";
+            }
+            else
+            {
+                label10.Visible = false;
+            }
+
+
+            // uzaleznienie temperatury od obrotow wentylatora
+            if (obroty > 1300)
+            {
+                temperatura -= 5;
+            }
+            else if (obroty < 1100)
+            {
+                temperatura += 5;
+            }
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -51,13 +120,13 @@ namespace Symulator_linii_produkcyjnej
         }
 
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)  // timer od losowych awarii
         {
             zegarawarii.Stop();
             MessageBox.Show("Nastąpiła nieprzewidziana awaria, proszę zresetuj linię produkcyjną");
         }
 
-        private void timer2_Tick(object sender, EventArgs e)
+        private void timer2_Tick(object sender, EventArgs e)  // timer od sprawdzania obecnosci
         {
             button1.Visible = true;
             if (!nieobecnosc)
@@ -69,10 +138,39 @@ namespace Symulator_linii_produkcyjnej
             }
             else
             {
+                zegarobecnosci.Stop();
+                zegarawarii.Stop();
                 MessageBox.Show("Uwaga wykryto nieobecność operatura, następuje automatyczne zatrzymanie linii produkcyjnej i wylogowanie użytkownika.");
                 Application.Restart();
             }
 
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)   // timer od symulacji zmiennych
+        {
+            zegarsymulacji.Stop();
+            Symulacja();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            obroty -= 50;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            obroty+= 50;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            cpu -= 25;
+            button5.Visible = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            cisnienie -= 40;
         }
     }
 }
